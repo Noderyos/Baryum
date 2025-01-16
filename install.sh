272a8d25de7df22d087901dcc3d5dbe497bdeff8
Noderyos <vincent.bsod@gmail.com>
2025-01-16 18:50:21 +0100

    Patching memory leaks

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 make -C src/sound
+make -C src/i3
+make -C src/current_app
 make -C src/time
 make
--- src/time/time.c
+++ src/time/time.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <time.h>
 #include <getopt.h>
-#include "concat.h"
 
 char *weekday_color = "{0000FF}";
 char *time_color = "{FFFFFF}";
@@ -16,26 +15,33 @@ void custom_time(char order[]) {
     char weekday[16], time_str[16], date_str[32];
     time_t now = time(NULL);
     struct tm *local = localtime(&now);
-    char *output = "";
+    char output[1024] = {0};
 
     for (int i = 0; i < 3; i++) {
         switch (order[i]) {
             case 'w': // WEEKDAY
-		strftime(weekday, sizeof(weekday), "%A", local);
-		output = concat(output, concat(weekday_color, weekday, reset_color), " ");	
+		        strftime(weekday, sizeof(weekday), "%A", local);
+		        strcat(output, weekday_color);
+		        strcat(output, weekday);
+                strcat(output, " ");
                 break;
             case 't': // TIME
-		strftime(time_str, sizeof(time_str), "%H:%M:%S", local);
-                output = concat(output, concat(time_color, time_str, reset_color), " ");
+		        strftime(time_str, sizeof(time_str), "%H:%M:%S", local);
+                strcat(output, time_color);
+		        strcat(output, time_str);
+                strcat(output, " ");
                 break;
-            case 63: // DATE (number ascii for d)
-		strftime(date_str, sizeof(date_str), "%e %b %Y", local);
-		output = concat(output, concat(date_color, date_str, reset_color), " ");
+            case 'd':
+		        strftime(date_str, sizeof(date_str), "%e %b %Y", local);
+		        strcat(output, date_color);
+		        strcat(output, weekday);
+                strcat(output, " ");
                 break;
             default:
                 break;
         }
     }
+    strcat(output, reset_color);
     printf("%s\n", output);
 }
 
@@ -43,10 +49,10 @@ int main(int argc, char *argv[]) {
     char order[3] = {0};
     int opt;
     int cpt = 0;
-    while ((opt = getopt(argc, argv, "wtd:")) != -1) {
+    while ((opt = getopt(argc, argv, "wtd")) != -1) {
 	    if(cpt > 3){
 	        fprintf(stderr, 
-		    "Usage: %s [-w] [-t] [-d]  \n", argv[0]);
+		    "Usage: %s [-w] [-t] [-d]\n", argv[0]);
     	    exit(EXIT_FAILURE);
 
 	    }
